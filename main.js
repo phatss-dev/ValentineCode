277c12adf516519646f1b836ea91406c9b6b57cb
phatss-dev <phunghuuphat3@gmail.com>
2026-05-31 22:12:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent = "Đây là nội dung của bức thư mà người ấy sẽ đọc được. Hãy viết gì đó cho người đó thôi nào. Cảm ơn bạn đã xem"
+var letterContent = "HÔM NAY LÀ 1/6 LÀ NGÀY QUỐC TẾ THIẾU NHI DO VỢ LÈ EM BÉ CỦA ANH NÊN ANH LÀM BỨC THƯ NÌ ẠA. ANH CHÚC BÉ IU CỦA ANH 1/6 THẬT NHÌU NIỀM VUI, THẬT NHIỀU TIẾNG CƯỜI VÀ THẬT NHÌU QUÀ NHAA! MONG BÉ IU LUÔN XINHH XẮN, ĐÁNG IU, NGOAN NGOÃN VÀ MŨI NGÀY ĐỀU TRÀN NGẬP HẠNH PHÚC. CHÚC BÉ IU LUÔN ĐẠT ĐƯỢC THÀNH CÔNG TRONG HỌC TẬP CŨNG NHƯ CUỘC SỐNG ẠAA! 🎁🎈🎉 "
 
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
